4cc41e5850948ceb112a81f83071564e5b1a7d47
trinadh Ravuri <trinadh.ravz@gmail.com>
2023-07-19 10:29:57 +0530

    home nav bar updated

--- dbpract/views.py
+++ dbpract/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+import random
+from django.http import JsonResponse
 # Create your views here.
 
 def home(request):
     return render(request,'home.html')
+
+def password(request):
+    char = list('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*_+\/><~')
+    password = ''
+    for i in range(15):
+        password += random.choice(char)
+    pas = {'password':password}
+    return JsonResponse(pas)
--- practicedatabase/settings.py
+++ practicedatabase/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'practicedatabase.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'dbpract/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'dbpract/static',
+    BASE_DIR / 'media'
+]
 
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
