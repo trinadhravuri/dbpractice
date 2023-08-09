28ccaf9a72b9f69bd5fa0e521293ac330b3582dd
trinadh Ravuri <trinadh.ravz@gmail.com>
2023-08-09 08:10:09 +0530

    pagination added

--- listings/admin.py
+++ listings/admin.py
@@ -3,8 +3,9 @@ from .models import Listings
 # Register your models here.
 
 class AdminListings(admin.ModelAdmin):
-    list_displa=('title','city')
+    list_display=('id','title','city')
+    list_display_links = ('id','title')
     list_filter=('bedrooms','bathrooms','sqft')
-
+    search_fields = ('title','description','city','address','Zipcode','price')
 
 admin.site.register(Listings,AdminListings)
--- listings/views.py
+++ listings/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import Listings
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 
 
 def listings_pro(request):
-    lists = Listings.objects.all()
+    lists = Listings.objects.order_by('-list_date')
+    paginator = Paginator(lists,1)
+    page = request.GET.get('page')
+    contents = paginator.get_page(page)
     context = {
-        'lists':lists,
+        'lists':contents
     }
     return render(request,'listings/listings.html',context)
